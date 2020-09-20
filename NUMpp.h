e9e1a22a2b71858419875a029d81308af2147610
tetex7 <57007495+tetex7@users.noreply.github.com>
2020-09-19 22:58:31 -0400

    Update NUMpp.h

--- NUMpp.h
+++ NUMpp.h
@@ -1,5 +1,11 @@
 #pragma once
-namespace NUMpp
-{
-	long int powr(int* in, int pow);
-}
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+	long int powr(int in, int pow);
+
+#ifdef __cplusplus
+	}
+#endif // __cplusplus
